fcea0dc73746ac05d553a5e18d71ce282c684bba
Nicolas Lalevee <hibou@apache.org>
2010-11-17 09:39:39 +0000

    test case for IVYDE-237
    git-svn-id: https://svn.apache.org/repos/asf/ant/ivy/ivyde/trunk@1035958 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/src/org/apache/ivyde/test/JettyLauncher.java
@@ -0,0 +1,38 @@
+package org.apache.ivyde.test;
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+import java.io.FileInputStream;
+
+import org.mortbay.jetty.Server;
+import org.mortbay.xml.XmlConfiguration;
+
+/**
+ * Basic jetty launcher
+ */
+public class JettyLauncher {
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server();
+        FileInputStream jettyConfFile = new FileInputStream(args[0]);
+        XmlConfiguration configuration = new XmlConfiguration(jettyConfFile);
+        jettyConfFile.close();
+        configuration.configure(server);
+        server.start();
+        server.join();
+    }
+}
