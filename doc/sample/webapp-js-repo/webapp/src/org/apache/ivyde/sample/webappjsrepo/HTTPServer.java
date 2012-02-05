0249447b680b8c02ca301e84158a9b5a425950f9
Nicolas Lalevee <hibou@apache.org>
2012-02-05 20:50:09 +0000

    Add a doc about the non-Java dependency management with IvyDE, with a sample
    git-svn-id: https://svn.apache.org/repos/asf/ant/ivy/ivyde/trunk@1240808 13f79535-47bb-0310-9956-ffa450edef68

---
+++ doc/sample/webapp-js-repo/webapp/src/org/apache/ivyde/sample/webappjsrepo/HTTPServer.java
@@ -0,0 +1,37 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.ivyde.sample.webappjsrepo;
+
+import java.net.InetSocketAddress;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.util.resource.Resource;
+
+public class HTTPServer {
+
+    public static void main(String[] args) throws Exception {
+        InetSocketAddress bindAdress = new InetSocketAddress("localhost", 8888);
+        Server server = new Server(bindAdress);
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setBaseResource(Resource.newClassPathResource("/org/apache/ivyde/sample/webappjsrepo/"));
+        server.setHandler(resourceHandler);
+        server.start();
+        server.join();
+    }
+}
