67edc141ee6c8eb2bad74b13e99add4c1a826e9c
Gintas Grigelionis <gintas@apache.org>
2017-10-29 22:23:46 +0100

    Add a missing brace

--- org.apache.ivyde.eclipse.resolvevisualizer/src/org/apache/ivyde/eclipse/resolvevisualizer/IvyNodeLabelProvider.java
+++ org.apache.ivyde.eclipse.resolvevisualizer/src/org/apache/ivyde/eclipse/resolvevisualizer/IvyNodeLabelProvider.java
@@ -88,7 +88,7 @@ public class IvyNodeLabelProvider implements ILabelProvider, IConnectionStylePro
         if (element instanceof IvyNodeElement) {
             IvyNodeElement node = (IvyNodeElement) element;
             String text = node.getOrganization() + "#" + node.getName() + ";";
-            if (node.getRevision().indexOf("working@") != -1)
+            if (node.getRevision().indexOf("working@") != -1) {
                 text += "WORKSPACE";
             } else {
                 text += node.getRevision();
